a478a665257c63e20ee4a6f51d46d2c12030df5e
tinabhowal <117632464+tinabhowal@users.noreply.github.com>
2023-03-13 12:11:28 +0100

    .

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -6,9 +6,7 @@ import { useDispatch } from 'react-redux';
 
 export const FavoriteMovies = () => {
 
-// const handleToggle = (movie) => {
-//         toggleFavorite(movie);
-//     };
+
 
 const user = useSelector((state) => state.user.user);
 const token = localStorage.getItem('token');
@@ -18,26 +16,7 @@ const favMovies = user.FavouriteMovies ?? [];
   const favMoviesList = favMovies && favMovies.length !== 0 ? movies.filter((m) => favMovies.includes(m.id)) : [];
     return (
     <>
-    {/* <Row>
-    <Col xs={12} className="movie-list">
-        <h2>My favorites</h2>
-    </Col>
-    </Row>
-    <Row>  
-                    {favoriteMovies.map((movie) => (
-                        <Col xs={12} md={6} lg={3} key={movie.id}>
-                        <MovieCard
-                            
-                            movie={movie}
-                            toggleFavorite={handleToggle}
-                            hasFavorite={true}
-                            
-                        />   
-                        </Col> 
-
-                    ))}
     
-    </Row> */}
 
 <Row>
       <div className='text-start h2 mb-4'>My Favorite Movies: </div>
